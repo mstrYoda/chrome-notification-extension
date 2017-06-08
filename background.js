da92254fcefb73eb05cd45a5770a98f6f4ffe1e5
mstrYoda <freelancemuhendis@gmail.com>
2017-06-08 23:29:06 +0300

    added messaging between popup and background

--- background.js
+++ background.js
@@ -1,22 +1,35 @@
 var adet = 0;
 
+var veriler = [];
+
 function createNotification(data){
-    var notOpt = {
-        type: 'basic',
-        title: 'Yeni veri eklendi !',
-        message: data.name,
-        iconUrl: 'icon.png'
+
+    veriler = veriler.concat(data);
+    chrome.storage.sync.set({'veriler': veriler}, function() {
+        console.log('veriler saved');
+    });
+
+    for(var i = 0; i<data.length; i++){
+        var notOpt = {
+            type: 'basic',
+            title: 'Yeni veri eklendi !',
+            message: data[i].site + ' - ' + data[i].zaman,
+            iconUrl: 'icon.png'
+        }
+
+        chrome.notifications.create('id5',notOpt,(id)=>{
+            adet+=1;
+            chrome.browserAction.setBadgeText({text: adet.toString()});
+        });
+
     }
 
-    chrome.notifications.create('id5',notOpt,(id)=>{
-        adet+=1;
-        chrome.browserAction.setBadgeText({text: adet.toString()});
-    });
+    
 }
 
 function baglan(){
 
-    fetch('http://localhost:3000/deneme')
+    fetch('http://localhost:5000/deneme')
     .then((data) =>{
         return data.json();
     })
@@ -30,10 +43,17 @@ function baglan(){
 
 }
 
-setInterval(baglan, 1500);
+setInterval(baglan, 2000);
 
 chrome.runtime.onMessage.addListener((resp, sender, senResp)=>{
-    createNotification();
+    if(resp.msg = 'sayac_sifirla'){
+        adet = 0;
+        chrome.browserAction.setBadgeText({text: "0"});
+        veriler = [];
+        chrome.storage.sync.set({'veriler': veriler}, function() {
+          console.log('veriler sifirlandi');
+        });
+    }
 });
 
 chrome.browserAction.setBadgeText({text: "0"});
--- popup.js
+++ popup.js
@@ -1,11 +1,38 @@
-var adet = 0;
+var veriler = [];
 
 function kaldir(id){
+    chrome.runtime.sendMessage({msg: 'sayac_sifirla'}, function(response) {
 
-    chrome.storage.sync.remove('bildirimler', function() {
-        console.log('kaldirildi');
     });
-
 }
 
 document.getElementById('send').addEventListener('click', kaldir);
+
+function messageReceived(data){
+    veriler = data;
+    var ul = document.getElementById('veriler');
+
+    var liste = '';
+
+    for(var i = 0; i< veriler.length; i++){
+        liste += `<li> ${veriler[i].site} </li>`;
+    }
+
+    ul.innerHTML = liste;
+
+}
+
+chrome.runtime.onMessage.addListener(messageReceived);
+
+ chrome.storage.sync.get('veriler', function(items) {
+    var ul = document.getElementById('veriler');
+    console.log(items);
+
+    var liste = '';
+
+    for(var i = 0; i< items.veriler.length; i++){
+        liste += `<li> ${items.veriler[i].site} </li>`;
+    }
+
+    ul.innerHTML = liste;
+});
